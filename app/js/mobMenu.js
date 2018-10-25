7dda7cfebe5ead2f8bdcba878526e53600caf9a5
Zoia R <zizi2405@yandex.ru>
2018-10-25 07:18:16 -0400

    add app

--- app/js/mobMenu.js
+++ app/js/mobMenu.js
@@ -1,1 +1,7 @@
-"use strict";!function(){var e=document.querySelector(".mob__menu_buttom"),t=document.querySelector(".menu");e.addEventListener("click",function(){t.classList.contains("menu__active")?t.classList.remove("menu__active"):t.classList.add("menu__active")})}();
+"use strict";!function(){var e=document.querySelector(".mob__menu_buttom"),t=document.querySelector(".menu");e.addEventListener("click",function(){t.classList.contains("menu__active")?t.classList.remove("menu__active"):t.classList.add("menu__active")})}();('menu__active')) {
+      mobMenu.classList.remove('menu__active');
+    } else {
+      mobMenu.classList.add('menu__active');
+    }
+  }
+}();
